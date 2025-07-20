f62522c47d18b7da5bb8c6e9eaab6c4aaed6fa96
Peter Ayad <eng.peter2013@gmail.com>
2025-07-20 14:23:57 -0700

    Create serializers to define the many to many relationships

---
+++ messaging_app/chats/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import User, Message, Conversation
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'user_id', 'username', 'first_name', 'last_name',
+            'email', 'phone_number', 'role'
+        ]
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = [
+            'message_id', 'sender', 'conversation',
+            'message_body', 'sent_at'
+        ]
+        read_only_fields = ['message_id', 'sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = [
+            'conversation_id', 'participants',
+            'created_at', 'messages'
+        ]
+
+    def get_messages(self, obj):
+        messages = Message.objects.filter(conversation=obj)
+        return MessageSerializer(messages, many=True).data
