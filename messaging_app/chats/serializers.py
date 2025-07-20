48537a60a407ca832df02574f7cb8b96fb52d9dd
Peter Ayad <eng.peter2013@gmail.com>
2025-07-20 14:27:59 -0700

    Create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
 from .models import User, Message, Conversation
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = [
             'user_id', 'username', 'first_name', 'last_name',
-            'email', 'phone_number', 'role'
+            'email', 'phone_number', 'role', 'password'
         ]
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Password must be at least 8 characters long.")
+        return make_password(value)
+
+    def create(self, validated_data):
+        return super().create(validated_data)
 
 
 class MessageSerializer(serializers.ModelSerializer):
